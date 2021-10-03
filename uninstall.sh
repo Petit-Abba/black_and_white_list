ca85559f821075054473ba4dd2867cbda8599219
Petit-Abba <1306144276@qq.com>
2021-10-03 12:53:21 +0800

    2021-10-12 12:53:20 auth commit

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/sbin/sh
 clear_the_blacklist_dir="/data/media/0/Android/clear_the_blacklist"
-if [ -d ${clear_the_blacklist_dir} ]; then
-  rm -rf ${clear_the_blacklist_dir}
+if [ -d $clear_the_blacklist_dir ]; then
+  rm -rf $clear_the_blacklist_dir
 fi
