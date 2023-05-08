d2b96444d043b0d084c629c78f3314acb372ec21
huanchen <xhc-xh@qq.com>
2023-05-08 20:23:22 +0800

    精简更新代码，去除一个功能提升运行效率，部分代码分至另一个sh文件内

--- script/Run_clear.sh
+++ script/Run_clear.sh
@@ -1,143 +1,104 @@
-
-# 跳过白名单次数判断
-print_pd_white_list() {
-  white_file="$MODDIR/White_List_File/$(echo "$1" | sed 's/\///g').ini"
-  if [[ ! -e "$white_file" ]]; then
-    echo "1" > "$white_file"
-    logd "[continue] -[$(cat "$white_file")]- $2"
-  else
-    echo "- [continue]: $1"
-    # 小于3次则打印
-    if [[ "$(cat "$white_file")" -lt "3" ]]; then
-      echo "$(($(cat "$white_file")+1))" > "$white_file"
-      logd "[continue] -[$(cat "$white_file")]- $2"
-    fi
-  fi
-}
-
-# 小米应用商店文件夹判断
-com_xiaomi_market() {
-  if [[ ! -z "$(echo "$1" | grep -w "com.xiaomi.market")" ]]; then
-    if [[ "$(find "$1" -name "*.apk")" != "" ]]; then
-      logd "存在APK: $1"
-      return 2
-    fi
-  fi
-}
-
-# 寻找black文件位置
-main_find_black() {
-  local IFS=$'\n'
-  find_black="$(find /data/media/ -type f -name 'black')"
-  if [[ ! -z $find_black ]]; then
-    identifier="$(cat $Black_List | grep -w '#black标识符')"
-    [[ -z $identifier ]] && echo "#black标识符" >> $Black_List
-    for black_file_path in $find_black; do
-      BLACK="${black_file_path%/black}"
-      if [[ ! -z "$(cat $White_List | grep "$BLACK")" ]]; then
-        logd "[continue black] --白名单DIR: $BLACK"
-        continue
-      fi
-      logd "[add black] --黑名单DIR: $BLACK/"
-      sed -i "/$identifier/a$BLACK/" "$Black_List"
-    done
-  fi
-}
-
-# 白名单列表通配符拓展
-whitelist_wildcard_list() {
-  local IFS=$'\n'
-  for wh in `cat $White_List | grep -v '#'`
-  do
-    echo "$wh"
-  done
-}
-
-# 黑名单列表通配符拓展
-blacklist_wildcard_list() {
-  local IFS=$'\n'
-  for bl in `cat $Black_List | grep -v '#'`
-  do
-    echo "$bl"
-  done
-}
-
+#全局变量
+MODDIR=${0%/*}
+#主程序
 main_for() {
   # 重新定义字段分隔符 忽略空格和制表符
   local IFS=$'\n'
   # 因为重新定义字段分隔符 所以只需要for 不再需要while read
-  for i in $(echo "$Black_List_Expand" | grep -v '*'); do
+  for i in $(echo "$Black_List_Expand" | grep -v -F '*'); do
     # 文件夹
     if [[ -d "$i" ]]; then
-      case $i in *'/.') continue ;; *'/./') continue ;; *'/..') continue ;; *'/../') continue ;; esac
-      if [[ ! -z $(echo "$White_List_Expand" | grep -w "$i") ]]; then
+      #跳过指定目录，防止危险
+      case $i in
+      *'/.') continue ;;
+      *'/./') continue ;;
+      *'/..') continue ;;
+      *'/../') continue ;;
+      esac
+      if echo "$White_List_Expand" | grep -qw "$i"; then
         print_pd_white_list "$i" "白名单DIR: $i"
         continue
       fi
       com_xiaomi_market "$i"
       [[ $? == 2 ]] && continue
       rm -rf "$i" && {
-        let DIR++
+        ((DIR++))
         logd "[rm] --黑名单DIR: $i"
-        echo "$DIR" > $tmp_date/dir
+        echo "$DIR" >"$tmp_date"/dir
       }
     fi
     # 文件
     if [[ -f "$i" ]]; then
-      if [[ ! -z $(echo "$White_List_Expand" | grep -w "$i") ]]; then
+      if echo "$White_List_Expand" | grep -qw "$i"; then
         print_pd_white_list "$i" "白名单FILE: $i"
         continue
       fi
       rm -rf "$i" && {
-        let FILE++ 
+        ((FILE++))
         logd "[rm] --黑名单FILE: $i"
-        echo "$FILE" > $tmp_date/file
+        echo "$FILE" >"$tmp_date"/file
       }
     fi
   done
 }
 
-MODDIR="${0%/*}"
+#拉起子sh进程
 . "$MODDIR/clear_the_blacklist_functions.sh"
+
+#读取配置文件
+conf="$black_and_white_list_path"/定时任务/定时设置.ini
+if [[ -f $conf ]]; then
+  if ! . "$conf"; then
+    logd "- [!]: 文件读取异常，请审查(设置定时.ini)文件内容！" && exit 1
+  fi
+else
+  logd "- [!]: 缺少$conf文件" && exit 2
+fi
+
+#获取配置
+if [[ $bigfile_auto_skip == y ]]; then
+  skip_mb="$bigfile_mb"
+else
+  skip_mb=-1
+fi
+
+#拉起模组进程
+. "$MODDIR/Run_clear_mod.sh"
 
 if [[ "$Screen" == "亮屏" ]]; then
   echo "- 亮屏状态"
   if [[ ! -f $MODDIR/tmp/Screen_on ]]; then
-    echo "true" > $MODDIR/tmp/Screen_on
+    echo "true" >"$MODDIR"/tmp/Screen_on
     logd "[状态]: [$Screen] 执行"
   fi
-  [[ ! -d $MODDIR/White_List_File ]] && mkdir -p $MODDIR/White_List_File
-  [[ ! -d $MODDIR/tmp/DATE ]] && mkdir -p $MODDIR/tmp/DATE
+  #创建白名单拦截次数记录文件
+  [[ ! -d $MODDIR/White_List_File ]] && mkdir -p "$MODDIR"/White_List_File
+  #创捷记录文件
+  [[ ! -d $MODDIR/tmp/DATE ]] && mkdir -p "$MODDIR"/tmp/DATE
   tmp_date="$MODDIR/tmp/DATE/$(date '+%Y%m%d')"
+  #判断是否是新的一天
   if [[ ! -d "$tmp_date" ]]; then
-    rm -rf $MODDIR/tmp/DATE/*/ >/dev/null 2>&1
-    mkdir -p $tmp_date
-    echo "0" > $tmp_date/file
-    echo "0" > $tmp_date/dir
-    # 文件大小
-    #filesize="$(ls -l ${log} | awk '{print $5}')"
-    # 3kb
-    #maxsize="$((1024*3))"
-    #[[ $filesize -gt $maxsize ]] && 
+    rm -rf "$MODDIR"/tmp/DATE/ &>/dev/null
+    mkdir -p "$tmp_date"
+    echo "0" >"$tmp_date"/file
+    echo "0" >"$tmp_date"/dir
     log_md_clear
   fi
-  FILE="$(cat $tmp_date/file)"
-  DIR="$(cat $tmp_date/dir)"
+  FILE="$(cat "$tmp_date"/file)"
+  DIR="$(cat "$tmp_date"/dir)"
 
-  main_find_black
   White_List_Expand="$(whitelist_wildcard_list)"
   Black_List_Expand="$(blacklist_wildcard_list)"
   # 执行方法
   main_for
 
-  FILE="$(cat $tmp_date/file)"
-  DIR="$(cat $tmp_date/dir)"
-  sed -i "/^description=/c description=CROND: [ 今日已清除: $FILE个黑名单文件 | $DIR个黑名单文件夹 ] - Repo: https://github.com/Petit-Abba/black_and_white_list/" "${MODDIR%/script}/module.prop"
+  FILE="$(cat "$tmp_date"/file)"
+  DIR="$(cat "$tmp_date"/dir)"
+  sed -i "/^description=/c description=CROND: [ 今日已清除: $FILE个黑名单文件 | $DIR个黑名单文件夹 ]" "${MODDIR%/script}/module.prop"
 else
   echo "- 息屏状态"
   if [[ -f $MODDIR/tmp/Screen_on ]]; then
-    rm -rf $MODDIR/tmp/Screen_on
+    rm -rf "$MODDIR"/tmp/Screen_on
     logd "[状态]: [$Screen] 不执行"
   fi
 fi
-
